1aeea927e8ddab03e58e5a76c1d42b438c5d44d3
marquetterobotics7826 <you@example.com>
2024-03-14 21:02:24 -0400

    LEDS

--- src/main/java/frc/robot/subsystems/LEDSubsystem/CTRELEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem/CTRELEDSubsystem.java
@@ -1,25 +1,44 @@
 package frc.robot.subsystems.LEDSubsystem;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LEDConstants;
 
 import com.ctre.phoenix.led.Animation;
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
+import com.ctre.phoenix.led.TwinkleAnimation;
 
 public class CTRELEDSubsystem extends SubsystemBase {
 
     private CANdle candle;
+    private CANdleConfiguration config;
 
     public CTRELEDSubsystem() {
         candle = new CANdle(LEDConstants.kLEDCANID);
+        this.config = new CANdleConfiguration();
+        this.config.brightnessScalar = 1;
+        this.config.statusLedOffWhenActive = true;
+        this.config.v5Enabled = false;
+        this.config.stripType = LEDStripType.RGB; 
+        SmartDashboard.putBoolean("Set LED Color", true);
+       
+    }
 
+    @Override
+    public void periodic() {
     }
 
     public void setSolidColor(int[] rgb) {
+        SmartDashboard.putBoolean("Set LED Color", true);
         candle.setLEDs(rgb[0], rgb[1], rgb[2]);
     }
 
     public void animate(Animation animation) {
-        candle.animate(animation);
+        candle.animate(new StrobeAnimation(0, 255, 0, 155, .5, 128));//new Animation() {
+            ;//FireAnimation(1, .5, 155, 128, 1));
     }
 }
